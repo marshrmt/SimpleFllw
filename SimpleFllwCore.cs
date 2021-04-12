2f13cb8258fc8b9674465e632eb25e6ee360ee14
vnz <marshrmt@gmail.com>
2021-04-12 21:38:13 +0300

    cleanup

--- SimpleFllwCore.cs
+++ SimpleFllwCore.cs
@@ -84,85 +84,6 @@ namespace SimpleFllw
 				if(!_areaTransitions.ContainsKey(transition.Id))
 					_areaTransitions.Add(transition.Id, transition);
 			}
-
-
-			var terrain = GameController.IngameState.Data.Terrain;
-			var terrainBytes = GameController.Memory.ReadBytes(terrain.LayerMelee.First, terrain.LayerMelee.Size);
-			_numCols = (int)(terrain.NumCols - 1) * 23;
-			_numRows = (int)(terrain.NumRows - 1) * 23;
-			if ((_numCols & 1) > 0)
-				_numCols++;
-
-			_tiles = new byte[_numCols, _numRows];
-			int dataIndex = 0;
-			for (int y = 0; y < _numRows; y++)
-			{
-				for (int x = 0; x < _numCols; x += 2)
-				{
-					var b = terrainBytes[dataIndex + (x >> 1)];
-					_tiles[x, y] = (byte)((b & 0xf) > 0 ? 1 : 255);
-					_tiles[x+1, y] = (byte)((b >> 4) > 0 ? 1 : 255);
-				}
-				dataIndex += terrain.BytesPerRow;
-			}
-
-			terrainBytes = GameController.Memory.ReadBytes(terrain.LayerRanged.First, terrain.LayerRanged.Size);
-			_numCols = (int)(terrain.NumCols - 1) * 23;
-			_numRows = (int)(terrain.NumRows - 1) * 23;
-			if ((_numCols & 1) > 0)
-				_numCols++;
-			dataIndex = 0;
-			for (int y = 0; y < _numRows; y++)
-			{
-				for (int x = 0; x < _numCols; x += 2)
-				{
-					var b = terrainBytes[dataIndex + (x >> 1)];
-
-					var current = _tiles[x, y];
-					if(current == 255)
-						_tiles[x, y] = (byte)((b & 0xf) > 3 ? 2 : 255);
-					current = _tiles[x+1, y];
-					if (current == 255)
-						_tiles[x + 1, y] = (byte)((b >> 4) > 3 ? 2 : 255);
-				}
-				dataIndex += terrain.BytesPerRow;
-			}
-
-
-			GeneratePNG();
-		}
-
-		public void GeneratePNG()
-		{
-			using (var img = new Bitmap(_numCols,_numRows))
-			{
-				for (int x = 0; x < _numCols; x++)
-					for (int y = 0; y < _numRows; y++)
-					{
-						try
-						{
-							var color = System.Drawing.Color.Black;
-							switch (_tiles[x, y])
-							{
-								case 1:
-									color = System.Drawing.Color.White;
-									break;
-								case 2:
-									color = System.Drawing.Color.Gray;
-									break;
-								case 255:
-									color = System.Drawing.Color.Black;
-									break;
-							}
-							img.SetPixel(x, y, color);
-						}
-						catch (Exception e)
-						{
-							Console.WriteLine(e);
-						}
-					}
-				img.Save("output.png");
-			}
 		}
 
 
@@ -304,8 +225,8 @@ namespace SimpleFllw
 					case TaskNodeType.Movement:
 						_nextBotAction = DateTime.Now.AddMilliseconds(Settings.BotInputFrequency + random.Next(Settings.BotInputFrequency));
 
-						if (Settings.IsDashEnabled && CheckDashTerrain(currentTask.WorldPosition.WorldToGrid()))
-							return null;
+						/*if (Settings.IsDashEnabled && CheckDashTerrain(currentTask.WorldPosition.WorldToGrid()))
+							return null;*/
 
 						Mouse.SetCursorPosHuman2(WorldToValidScreenPosition(currentTask.WorldPosition));
 						Thread.Sleep(random.Next(25) + 30);
@@ -391,73 +312,7 @@ namespace SimpleFllw
 			_lastPlayerPosition = GameController.Player.Pos;
 			return null;
 		}
-
-		private bool CheckDashTerrain(Vector2 targetPosition)
-		{		
-
-			//TODO: Completely re-write this garbage. 
-			//It's not taking into account a lot of stuff, horribly inefficient and just not the right way to do this.
-			//Calculate the straight path from us to the target (this would be waypoints normally)
-			var distance = Vector2.Distance(GameController.Player.GridPos,targetPosition);
-			var dir = targetPosition - GameController.Player.GridPos;
-			dir.Normalize();
-
-			var distanceBeforeWall = 0;
-			var distanceInWall = 0;
-
-			var shouldDash = false;
-			var points = new List<System.Drawing.Point>();
-			for (var i = 0; i < 500; i++)
-			{
-				var v2Point = GameController.Player.GridPos + i * dir;
-				var point = new System.Drawing.Point((int)(GameController.Player.GridPos.X + i * dir.X),
-					(int)(GameController.Player.GridPos.Y + i * dir.Y));
-
-				if (points.Contains(point))
-					continue;
-				if (Vector2.Distance(v2Point,targetPosition) < 2)
-					break;
-
-				points.Add(point);
-				var tile = _tiles[point.X, point.Y];
-
-
-				//Invalid tile: Block dash
-				if (tile == 255)
-				{
-					shouldDash = false;
-					break;
-				}
-				else if (tile == 2)
-				{
-					if (shouldDash)
-						distanceInWall++;
-					shouldDash = true;
-				}
-				else if (!shouldDash)
-				{
-					distanceBeforeWall++;
-					if (distanceBeforeWall > 10)					
-						break;					
-				}
-			}
-
-			if (distanceBeforeWall > 10 || distanceInWall < 5)
-				shouldDash = false;
-
-			if (shouldDash)
-			{
-				_nextBotAction = DateTime.Now.AddMilliseconds(500 + random.Next(Settings.BotInputFrequency));
-				Mouse.SetCursorPos(WorldToValidScreenPosition(targetPosition.GridToWorld(_followTarget == null ? GameController.Player.Pos.Z : _followTarget.Pos.Z)));
-				Thread.Sleep(50 + random.Next(Settings.BotInputFrequency));
-				Input.KeyDown(Settings.DashKey);
-				Thread.Sleep(15 + random.Next(Settings.BotInputFrequency));
-				Input.KeyUp(Settings.DashKey);
-				return true;
-			}
-
-			return false;
-		}
+		
 
 		private Entity GetFollowingTarget()
 		{
@@ -537,23 +392,21 @@ namespace SimpleFllw
 
 		public override void Render()
 		{
-			if (_tasks != null && _tasks.Count > 1)
-				for (var i = 1; i < _tasks.Count; i++)
-				{
-					var start = WorldToValidScreenPosition(_tasks[i - 1].WorldPosition);
-					var end = WorldToValidScreenPosition(_tasks[i].WorldPosition);
-					Graphics.DrawLine(start, end, 2, SharpDX.Color.Pink);
-				}
-			var dist = _tasks.Count > 0 ? Vector3.Distance(GameController.Player.Pos, _tasks.First().WorldPosition): 0;
-			var targetDist = _lastTargetPosition == null ? "NA" : Vector3.Distance(GameController.Player.Pos, _lastTargetPosition).ToString();
-			Graphics.DrawText($"Follow Enabled: {Settings.IsFollowEnabled.Value}", new Vector2(500, 120));
-			Graphics.DrawText($"Task Count: {_tasks.Count} Next WP Distance: {dist} Target Distance: {targetDist}", new Vector2(500, 140));
-			var counter = 0;
-			foreach (var transition in _areaTransitions)
+			Color followColor = Color.FromRgba(0xFF0000AA);
+			Color keyColor = Color.FromRgba(0xFF0000AA);
+
+			if (Settings.IsFollowEnabled.Value) 
 			{
-				counter++;
-				Graphics.DrawText($"{transition.Key} at { transition.Value.Pos.X} { transition.Value.Pos.Y}", new Vector2(100, 120 + counter * 20));
+				followColor = Color.FromRgba(0x00FF00AA);
 			}
+
+			if (Input.GetKeyState(Settings.MovementKey))
+			{
+				keyColor = Color.FromRgba(0x00FF00AA);
+			}
+
+			Graphics.DrawText($"FLLW", new Vector2(500, 120), followColor, 64);
+			Graphics.DrawText($"KEY", new Vector2(500, 420), keyColor, 64);
 		}
 
 
