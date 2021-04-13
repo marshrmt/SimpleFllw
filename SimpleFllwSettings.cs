36f769e813aeaac440339bdc89eb62aed6db8fe8
vnz <marshrmt@gmail.com>
2021-04-14 00:14:35 +0300

    follow correction by slot

--- SimpleFllwSettings.cs
+++ SimpleFllwSettings.cs
@@ -15,7 +15,7 @@ namespace SimpleFllw
 		[Menu("Move CMD Frequency")]public RangeNode<int> BotInputFrequency { get; set; } = new RangeNode<int>(50, 10, 250);
 		[Menu("Stop Path Distance")] public RangeNode<int> ClearPathDistance { get; set; } = new RangeNode<int>(740, 100, 5000);
 		[Menu("Follow Offset Direction")] public RangeNode<int> FollowOffsetDirection { get; set; } = new RangeNode<int>(90, 0, 200);
-		[Menu("Follow Offset Normal")] public RangeNode<int> FollowOffsetNormal { get; set; } = new RangeNode<int>(-150, 0, 150);
+		[Menu("Follow Offset Normal")] public RangeNode<int> FollowOffsetNormal { get; set; } = new RangeNode<int>(60, -150, 150);
 		[Menu("Follow Target Name")] public TextNode LeaderName { get; set; } = new TextNode("");
 		[Menu("Movement Key")] public HotkeyNode MovementKey { get; set; } = Keys.T;
 		[Menu("Allow Dash")] public ToggleNode IsDashEnabled { get; set; } = new ToggleNode(true);
